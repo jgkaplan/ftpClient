65eb8cf3637edfe41fd88a98c402a00f98e614f1
jgkaplan <joshuagraykaplan@gmail.com>
2017-07-06 19:22:03 -0500

    Tray now appears, wrong icon

--- main.js
+++ main.js
@@ -1,48 +1,90 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const Tray = electron.Tray;
+const {app, BrowserWindow, ipcMain, Tray, Menu} = require('electron');
 
 const path = require('path');
 const fs = require('fs');
-const url = require('url');
+const settings = require('settings.json');
 
-let win, tray;
+let mainWin, tray, trayWin;
 
-function createWindow() {
-    win = new BrowserWindow({width: 800, height: 600});
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+function createMainWindow() {
+    mainWin = new BrowserWindow({width: 800, height: 600, fullscreenable: false});
+    mainWin.loadURL(`file://${path.join(__dirname,'index.html')}`);
 
-    win.on('closed', () => {
-        win = null;
+    mainWin.on('closed', () => {
+        mainWin = null;
     });
 }
 
-// app.on('ready', createWindow);
-app.on('ready', function(){
-    tray = new Tray(path.join(__dirname,'images','menuicon'));
-    const contextMenu = Menu.buildFromTemplate([
-        {label: 'Item1', type: 'radio'},
-        {label: 'Item2', type: 'radio'},
-        {label: 'Item3', type: 'radio', checked: true},
-        {label: 'Item4', type: 'radio'}
-    ]);
-    tray.setToolTip('FTP Client');
-    tray.setContextMenu(contextMenu);
-});
+function makeTray(){
+    tray = new Tray(path.join(__dirname,'images','menuicon2.png'));
+    // const contextMenu = Menu.buildFromTemplate([
+    //     {label: 'Item1', type: 'radio'},
+    //     {label: 'Item2', type: 'radio'},
+    //     {label: 'Item3', type: 'radio', checked: true},
+    //     {label: 'Item4', type: 'radio'}
+    // ]);
+    // tray.setToolTip('FTP Client');
+    // tray.setContextMenu(contextMenu);
+    trayWin = new BrowserWindow({
+        width: 350,
+        height: 550,
+        show: false,
+        frame: false,
+        resizable: false,
+        fullscreenable: false
+    });
+    trayWin.loadURL(`file://${path.join(__dirname,'trayWindow.html')}`);
+    tray.on('right-click', function(event) {
+        toggleWindow();
+    });
+    tray.on('double-click', function(event) {
+        toggleWindow();
+    });
+    tray.on('click', function(event) {
+        toggleWindow();
+    });
+};
 
-app.on('window-all-closed', function(){
-    if(process.platform !== 'darwin'){
-        app.quit();
-    }
-});
+function toggleWindow() {
+	if (trayWin.isVisible()) {
+		trayWin.hide();
+	} else {
+		showWindow();
+	}
+}
+
+function showWindow() {
+	const trayPos = tray.getBounds();
+	const windowPos = trayWin.getBounds();
+	let x, y = 0;
+	if (process.platform == 'darwin') {
+		x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2));
+		y = Math.round(trayPos.y + trayPos.height);
+	} else {
+		x = Math.round(trayPos.x + (trayPos.width / 2) - (windowPos.width / 2));
+		y = Math.round(trayPos.y + trayPos.height * 10);
+	}
+	trayWin.setPosition(x, y, false);
+	trayWin.show();
+	trayWin.focus();
+}
+
+//Don't show in the dock
+if(settings.hide_dock_icon){
+    app.dock.hide();
+}
+
+app.on('ready', makeTray);
+
+//For now, keeping app active after all windows are closed because of trays
+// app.on('window-all-closed', function(){
+//     if(process.platform !== 'darwin'){
+//         app.quit();
+//     }
+// });
 
 app.on('activate', function(){
-    if(win == null){
-        createWindow();
+    if(mainWin == null){
+        createMainWindow();
     }
 });
